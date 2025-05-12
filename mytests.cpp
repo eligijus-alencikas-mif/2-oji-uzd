6f6d8011090cde0d70ebc3edad9881fb9896eb69
Eligijus Alencikas <eligijus.alencikas@mif.stud.vu.lt>
2025-05-12 19:05:13 +0300

    add one more test

--- mytests.cpp
+++ mytests.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 #include "student_test.h"
+#include "inputs.h"
+#include "constant_definitions.h"
+#include <iostream>
 
 TEST(TestStudentOperators, Destructor)
 {
@@ -28,6 +31,22 @@ TEST(TestStudentOperators, InputOperator)
 TEST(TestStudentOperators, OutputOperator)
 {
     EXPECT_TRUE(StudentTest::test_output_operator());
+}
+TEST(StudentCalculations, CalculateGrades)
+{
+    std::vector<Student> students;
+    students.push_back(Student("John", "Doe", {9, 8, 7}, 8));
+    students.push_back(Student("Jane", "Smith", {10, 9, 8}, 9));
+    students.push_back(Student("Alice", "Johnson", {4, 6, 7, 8, 9}, 6));
+
+    Calc_Students::calc_grades(students);
+
+    EXPECT_DOUBLE_EQ(students[0].get_final_score_avg(), HW_WEIGHT * 8 + EXAM_WEIGHT * 8);
+    EXPECT_DOUBLE_EQ(students[1].get_final_score_avg(), HW_WEIGHT * 9 + EXAM_WEIGHT * 9);
+    EXPECT_DOUBLE_EQ(students[2].get_final_score_avg(), HW_WEIGHT * 6.8 + EXAM_WEIGHT * 6);
+    EXPECT_DOUBLE_EQ(students[0].get_final_score_med(), 8.0);
+    EXPECT_DOUBLE_EQ(students[1].get_final_score_med(), 9.0);
+    EXPECT_DOUBLE_EQ(students[2].get_final_score_med(), 6.5);
 }
 
 int main(int arg, char **argv)
